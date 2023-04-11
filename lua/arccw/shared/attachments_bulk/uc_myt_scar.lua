3fc111bbb7cddb0634f960c36c3996ecee615962
rzen1th <pierce4077m@gmail.com>
2023-04-11 11:55:12 -0500

    caliber int sounds

--- lua/arccw/shared/attachments_bulk/uc_myt_scar.lua
+++ lua/arccw/shared/attachments_bulk/uc_myt_scar.lua
@@ -12,7 +12,7 @@ att.Desc_Cons = {}
 att.Desc_Neutrals = {}
 att.Slot = "uc_myt_scar_mag"
 
-att.Mult_RPM = 1.25 -- 650 rpm
+att.Mult_RPM = 1.25 -- 750 rpm
 
 att.AutoStats = true
 att.HideIfBlocked = true
@@ -25,6 +25,7 @@ local path = ")weapons/uc_osk/"
 local common = ")/arccw_uc/common/"
 local tail9 = ")/arccw_uc/common/9x19/"
 local fire9dist = {tail9 .. "fire-dist-9x19-pistol-ext-01.ogg", tail9 .. "fire-dist-9x19-pistol-ext-02.ogg", tail9 .. "fire-dist-9x19-pistol-ext-03.ogg", tail9 .. "fire-dist-9x19-pistol-ext-04.ogg", tail9 .. "fire-dist-9x19-pistol-ext-05.ogg", tail9 .. "fire-dist-9x19-pistol-ext-06.ogg"}
+local fire9distint = {tail9 .. "fire-dist-9x19-pistol-int-01.ogg", tail9 .. "fire-dist-9x19-pistol-int-02.ogg", tail9 .. "fire-dist-9x19-pistol-int-03.ogg", tail9 .. "fire-dist-9x19-pistol-int-04.ogg", tail9 .. "fire-dist-9x19-pistol-int-05.ogg", tail9 .. "fire-dist-9x19-pistol-int-06.ogg"}
 local fire9 = {path .. "fire-9-01.ogg", path .. "fire-9-02.ogg", path .. "fire-9-03.ogg", path .. "fire-9-04.ogg", path .. "fire-9-05.ogg", path .. "fire-9-06.ogg"}
 local fire9supp = {path .. "fire-9-sup-01.ogg", path .. "fire-9-sup-02.ogg", path .. "fire-9-sup-03.ogg", path .. "fire-9-sup-04.ogg", path .. "fire-9-sup-05.ogg", path .. "fire-9-sup-06.ogg"}
 
@@ -41,6 +42,14 @@ att.Hook_GetDistantShootSoundOutdoors = function(wep, distancesound)
         -- fallback to script
     else
         return fire9dist
+    end
+end
+
+att.Hook_GetDistantShootSoundIndoors = function(wep, distancesound)
+    if wep:GetBuff_Override("Silencer") then
+        -- fallback to script
+    else
+        return fire9distint
     end
 end
 
@@ -70,6 +79,7 @@ att.ActivateElements = {"conv_ak"}
 
 local tail762 = ")/arccw_uc/common/762x39/"
 local fire762dist = {tail762 .. "fire-dist-762x39-rif-ext-01.ogg", tail762 .. "fire-dist-762x39-rif-ext-02.ogg", tail762 .. "fire-dist-762x39-rif-ext-03.ogg", tail762 .. "fire-dist-762x39-rif-ext-04.ogg", tail762 .. "fire-dist-762x39-rif-ext-05.ogg", tail762 .. "fire-dist-762x39-rif-ext-06.ogg"}
+local fire762distint = {tail762 .. "fire-dist-762x39-rif-int-01.ogg", tail762 .. "fire-dist-762x39-rif-int-02.ogg", tail762 .. "fire-dist-762x39-rif-int-03.ogg", tail762 .. "fire-dist-762x39-rif-int-04.ogg", tail762 .. "fire-dist-762x39-rif-int-05.ogg", tail762 .. "fire-dist-762x39-rif-int-06.ogg"}
 local fire762 = {path .. "fire-762-01.ogg", path .. "fire-762-02.ogg", path .. "fire-762-03.ogg", path .. "fire-762-04.ogg", path .. "fire-762-05.ogg", path .. "fire-762-06.ogg"}
 local fire762supp = {path .. "fire-762-sup-01.ogg", path .. "fire-762-sup-02.ogg", path .. "fire-762-sup-03.ogg", path .. "fire-762-sup-04.ogg", path .. "fire-762-sup-05.ogg", path .. "fire-762-sup-06.ogg"}
 
@@ -86,6 +96,14 @@ att.Hook_GetDistantShootSoundOutdoors = function(wep, distancesound)
         -- fallback to script
     else
         return fire762dist
+    end
+end
+
+att.Hook_GetDistantShootSoundIndoors = function(wep, distancesound)
+    if wep:GetBuff_Override("Silencer") then
+        -- fallback to script
+    else
+        return fire762distint
     end
 end
 
@@ -117,6 +135,7 @@ att.ActivateElements = {"conv_556"}
 
 local tail556 = ")/arccw_uc/common/556x45/"
 local fire556dist = {tail556 .. "fire-dist-556x45-rif-ext-01.ogg", tail556 .. "fire-dist-556x45-rif-ext-02.ogg", tail556 .. "fire-dist-556x45-rif-ext-03.ogg", tail556 .. "fire-dist-556x45-rif-ext-04.ogg", tail556 .. "fire-dist-556x45-rif-ext-05.ogg", tail556 .. "fire-dist-556x45-rif-ext-06.ogg"}
+local fire556distint = {tail556 .. "fire-dist-556x45-rif-int-01.ogg", tail556 .. "fire-dist-556x45-rif-int-02.ogg", tail556 .. "fire-dist-556x45-rif-int-03.ogg", tail556 .. "fire-dist-556x45-rif-int-04.ogg", tail556 .. "fire-dist-556x45-rif-int-05.ogg", tail556 .. "fire-dist-556x45-rif-int-06.ogg"}
 local fire556 = {path .. "fire-556-01.ogg", path .. "fire-556-02.ogg", path .. "fire-556-03.ogg", path .. "fire-556-04.ogg", path .. "fire-556-05.ogg", path .. "fire-556-06.ogg"}
 local fire556supp = {path .. "fire-556-sup-01.ogg", path .. "fire-556-sup-02.ogg", path .. "fire-556-sup-03.ogg", path .. "fire-556-sup-04.ogg", path .. "fire-556-sup-05.ogg", path .. "fire-556-sup-06.ogg"}
 
@@ -133,6 +152,14 @@ att.Hook_GetDistantShootSoundOutdoors = function(wep, distancesound)
         -- fallback to script
     else
         return fire556dist
+    end
+end
+
+att.Hook_GetDistantShootSoundIndoors = function(wep, distancesound)
+    if wep:GetBuff_Override("Silencer") then
+        -- fallback to script
+    else
+        return fire556distint
     end
 end
 
